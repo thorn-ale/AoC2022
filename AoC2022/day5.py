f723dce99e6b30866830236d1a1548894e5e1e80
thorn-ale <thornale1@gmail.com>
2022-12-05 10:08:57 +0100

    day 5

--- AoC2022/day5.py
+++ AoC2022/day5.py
@@ -1,8 +1,77 @@
+import re
+from dataclasses import dataclass
 from dataloader import get_input_data
+
+
+def parse_stack(data: str) -> dict[int, list[str]]:
+    print(data)
+    result = {i:[] for i in range (1, 10)}
+    raw_stack = data.split('\n')
+    raw_stack.reverse()
+    for line in raw_stack:
+        if line.startswith(' 1'): continue
+        for i in range(9):
+            #0123456789
+            #[F] [L] [H] [R] [Z] [J] [J] [D] [D]
+            indexes = [1, 5, 9, 13, 17, 21, 25, 29, 33]
+            crate_id = line[indexes[i]]
+            if crate_id != ' ':
+                result[i+1].append(crate_id)
+    for k, v in result.items():
+        print(f'{k} => {v}')
+    return result
+
+
+@dataclass
+class Intstruction:
+    qt: int
+    src: int
+    dst: int
+
+def parse_orders(data: str) -> list[Intstruction]:
+    result = []
+    for line in data.split('\n'):
+        if not line.startswith('move'):
+            continue
+        result.append(Intstruction(*[int(x) for x in re.findall('[0-9]+', line)]))
+    return result
+
+def play_order(order: Intstruction, stacks: dict[int, list[str]]) -> dict[int, list[str]]:
+    for _ in range(order.qt):
+        temp = stacks[order.src].pop()
+        stacks[order.dst].append(temp)
+    return stacks
+
+def play_order_p2(order: Intstruction, stacks: dict[int, list[str]]) -> dict[int, list[str]]:
+    temp = stacks[order.src][-order.qt:]
+    stacks[order.dst] += temp
+    for _ in range(order.qt):
+        stacks[order.src].pop()
+    return stacks
 
 
 def main():
     data = get_input_data(5)
+    stacks = parse_stack(data[:322])
+    stacks2 = parse_stack(data[:322])
+    orders = parse_orders(data)
+    for o in orders:
+        stacks = play_order(o, stacks)
+        stacks2 = play_order_p2(o, stacks2)
+
+    print('result 1: ')
+    acc = ''
+    for k, v in stacks.items():
+        acc += v[-1]
+        print(f'{k} => {v}')
+    print(acc)
+
+    print('result 2: ')
+    acc2 = ''
+    for k, v in stacks2.items():
+        acc2 += v[-1]
+        print(f'{k} => {v}')
+    print(acc2)
 
 
 if __name__ == '__main__':
