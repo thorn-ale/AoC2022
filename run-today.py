81dc2288a5a3e16875e2f41af838db44943fe9d5
thorn-ale <thornale1@gmail.com>
2022-12-06 11:16:45 +0100

    improve tooling

--- run-today.py
+++ run-today.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+from pathlib import Path
 import subprocess
 import sys
 
 
 def main(args):
     day = datetime.now().day if len(args) == 1 else int(args[1])
-    subprocess.run(['python', f'./AoC2022/day{day}.py'])
+    project_dir = Path(__file__).parent
+    subprocess.run(['python', str(project_dir.joinpath(f'AoC2022/day{day}.py'))])
 
 
 if __name__ == '__main__':
