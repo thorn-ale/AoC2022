d0a0244ba2b06705152b0b5b3e5d5bda3e107171
thorn-ale <thornale1@gmail.com>
2022-12-06 11:06:02 +0100

    day 6

--- AoC2022/day6.py
+++ AoC2022/day6.py
@@ -1,8 +1,20 @@
 from dataloader import get_input_data
 
 
+def first_marker(data, size):
+    acc = ''
+    for i, char in enumerate(data):
+        if not ord(char) in range(ord('a'),ord('z')+1):
+            continue
+        acc += char
+        if len(acc) >= size and len(set(acc[-size:])) == size:
+            return (i+1, acc[-size:], set(acc[-size:]))
+                
+
 def main():
     data = get_input_data(6)
+    print(first_marker(data, 4))
+    print(first_marker(data, 14))
 
 
 if __name__ == '__main__':
