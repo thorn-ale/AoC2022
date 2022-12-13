d263936ac7739b063a60f8c500e63c53c3083df8
thorn-ale <thornale1@gmail.com>
2022-12-13 09:41:33 +0100

    day 12 and day 13

--- AoC2022/day12.py
+++ AoC2022/day12.py
@@ -1,8 +1,56 @@
 from dataloader import get_input_data
+from collections import deque
+import math
+
+
+def search_pos(hmap, width, heigt, target):
+    for y in range(heigt):
+        for x in range(width):
+            if hmap[y][x] == target:
+                return y,x
+
+
+def find_path(hmap, starts, height, width):
+    distances = {}
+    visited = deque([(0, s) for s in starts])
+
+    while len(visited) > 0:
+        t, p = visited.popleft()
+        if p in distances.keys():
+            continue
+        distances[p] = t
+        for n_y, n_x in [(p[0]-1, p[1]), (p[0]+1, p[1]), (p[0], p[1]-1), (p[0], p[1]+1)]:
+            if n_y < 0 or n_y >= height or n_x < 0 or n_x >= width:
+                continue
+            elv = hmap[p[0]][p[1]]
+            if elv == 'S':
+                elv = 'a'
+            if elv == 'E':
+                elv = 'z'
+            n_elv = hmap[n_y][n_x]
+            if n_elv == 'S':
+                n_elv = 'a'
+            if n_elv == 'E':
+                n_elv = 'z'
+            if ord(n_elv) - ord(elv) > 1:
+                continue
+            visited.append((t+1, (n_y, n_x)))
+    return distances
 
 
 def main():
     data = get_input_data(12)
+    hmap = [[x for x in y] for y in data.split('\n') if y]
+    width = len(hmap[0])
+    height = len(hmap)
+    start_pos = search_pos(hmap, width, height, 'S')
+    target = search_pos(hmap, width, height, 'E')
+    distances = find_path(hmap, [start_pos], height, width)
+    print(distances[target])
+    starts = [(y, x) for y in range(height) for x in range(width) if hmap[y][x] == 'a']
+    n_dist = find_path(hmap, starts, height, width).get(target, math.inf)
+    print(n_dist)
+    
 
 
 if __name__ == '__main__':
--- AoC2022/day13.py
+++ AoC2022/day13.py
@@ -1,9 +1,48 @@
 from dataloader import get_input_data
+from functools import reduce
+
+
+class Pair:
+    def __init__(self, l, r) -> None:
+        self.left = l
+        self.right = r
+
+
+def compare(val_left, val_right) -> int:
+    match val_left, val_right:
+        case int(val_left), int(val_right):
+            return val_left - val_right
+        case list(val_left), list(val_right):
+            for x, y in zip(val_left, val_right):
+                res = compare(x, y)
+                if res:
+                    return res
+            return len(val_left) - len(val_right)
+        case list(val_left), int(val_right):
+            return compare(val_left, [val_right])
+        case int(val_left), list(val_right):
+            return compare([val_left], val_right)
+
+
+class Packet:
+    def __init__(self, pval):
+        self.pval = pval
+
+    def __lt__(self, other):
+        return compare(self.pval, other.pval) < 0
+
+    def __eq__(self, other):
+        return compare(self.pval, other.pval) == 0
 
 
 def main():
     data = get_input_data(13)
-
+    pairs = [Pair(eval(x.split('\n')[0]), eval(x.split('\n')[1])) for x in data.split('\n\n') if x]
+    print(sum([i+1 for i, p in enumerate(pairs) if compare(p.left, p.right)<0]))
+    p2 = Packet([[2]])
+    p6 = Packet([[6]])
+    packets = sorted([Packet(eval(x)) for x in data.split('\n') if x] + [p2] + [p6])
+    print(reduce(int.__mul__, [i+1 for i, p in enumerate(packets) if p in [p2,p6]]))
 
 if __name__ == '__main__':
     main()
