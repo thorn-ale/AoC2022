4a623a75c8b01f6af6475af38b193fb2d7a5d5d2
thorn-ale <thornale1@gmail.com>
2022-12-16 12:33:50 +0100

    day 15 and day 16

--- AoC2022/day15.py
+++ AoC2022/day15.py
@@ -1,10 +1,95 @@
 from dataloader import get_input_data
+from dataclasses import dataclass
+import re
+from z3 import *
 
 
-def main():
+@dataclass
+class Point:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
+    def __repr__(self) -> str:
+        return f'({self.x}, {self.y})'
+
+def dist(p1: Point, p2: Point) -> int:
+    return abs(p1.x - p2.x)+abs(p1.y - p2.y)
+
+
+@dataclass
+class Reading:
+    sensor: Point
+    beacon: Point
+
+    def sensor_range(self) -> int:
+        return dist(self.sensor, self.beacon)
+
+    def __repr__(self) -> str:
+        return f'{self.sensor} -> {self.beacon}'
+
+def parse_reading(line: str) -> Reading:
+    coords = [int(x) for x in re.findall(r'-?\d+', line)]
+    return Reading(Point(coords[0], coords[1]), Point(coords[2], coords[3]))
+
+
+def part1():
     data = get_input_data(15)
+    readings = [parse_reading(x) for x in data.split('\n') if x]
+    x_max = max([r.beacon.x for r in readings]) + max([r.sensor_range() for r in readings])
+    x_min = min([r.beacon.x for r in readings]) - max([r.sensor_range() for r in readings])
+    target_y = 2000000
+    nb = 0
+    beacons = set([x.beacon for x in readings])
+    
+    for x in range(x_min, x_max):
+        in_range = False
+        p = Point(x, target_y)
+        for i, r in enumerate(readings):
+            d = dist(p, r.sensor)
+            if d <= r.sensor_range():
+                in_range = True
+                break
+        if in_range and p not in beacons:
+            nb += 1
+    print(nb)
+
+
+def part2():
+    data = get_input_data(15)
+    readings = [parse_reading(x) for x in data.split('\n') if x]
+    limit = 4000000
+    s = Solver()
+    x, y = Ints('x, y')
+    s.add(x >= 0)
+    s.add(y >= 0)
+    s.add(x <= limit)
+    s.add(y <= limit)
+
+    def Abs(x):
+        return If( x >= 0, x, -x)
+
+    for r in readings:
+        s.add(Abs(x - r.sensor.x) + Abs(y - r.sensor.y) > r.sensor_range())
+        s.add(And(x != r.beacon.x, y != r.beacon.y))
+    
+    assert s.check() == sat
+    res = s.model()
+    print(res[x].as_long() * limit + res[y].as_long())
 
 
 if __name__ == '__main__':
-    main()
+    part1()
+    part2()
 
--- AoC2022/day16.py
+++ AoC2022/day16.py
@@ -1,8 +1,63 @@
 from dataloader import get_input_data
+from dataclasses import dataclass
+import math
+import re
+from functools import cache
+
+
+@dataclass
+class Valve:
+    name: str
+    rate: int
+    links: list['str']
+
+    def score(self) -> int:
+        return self.rate * len(self.links)
+
+
+def parse_data(data: str):
+    vs = re.findall(r'[A-Z]{2}', data)
+    rate = int(re.findall(r'\d+', data)[0])
+    return Valve(vs[0], rate, vs[1:])
+
+
+@dataclass
+class WalkParams:
+    current: str
+    valves: dict[str, Valve]
+    dists: dict[str:[dict[str,int]]]
+    rt: int
+    elephant: bool = False
+
+    def __hash__(self) -> int:
+        hv = frozenset([(k,v.name) for k,v in self.valves.items()])
+        return hash((self.current, hv, self.rt, self.elephant))
+
+
+@cache
+def walk(wp: WalkParams) -> int:
+    res = walk(WalkParams('AA', wp.valves, wp.dists, 26)) if wp.elephant else 0
+    for v in wp.valves.keys():
+        move = wp.rt - wp.dists[wp.current][v] - 1
+        if move >= 0:
+            res = max(res, wp.valves[v].rate * move + walk(WalkParams(
+                v, {k: v2 for k, v2 in wp.valves.items() if k != v}, wp.dists, move, wp.elephant)))
+    return res
 
 
 def main():
     data = get_input_data(16)
+    valves = [parse_data(x) for x in data.split('\n') if x]
+    dists = {v.name:{x.name: 1 if x.name in v.links else 0 if x.name == v.name else math.inf for x in valves} for v in valves}
+    names = [x.name for x in valves]
+    valves = {v.name: v for v in valves if v.rate > 0}
+    for i in names:
+        for j in names:
+            for k in names:
+                dists[j][k] = min(dists[j][k], dists[j][i] + dists[i][k])
+
+    print(walk(WalkParams('AA', valves, dists, 30)))
+    print(walk(WalkParams('AA', valves, dists, 26, True)))
 
 
 if __name__ == '__main__':
